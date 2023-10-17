8211f0aa0ae1638cfe3a14fba77fbd13af96a9d8
k-hatori <hatori_k@awake-i.com>
2023-10-17 15:54:05 +0900

    リファクタリングVer2完了

--- app/src/main/java/com/example/filedownloaderHatori/MainActivity.kt
+++ app/src/main/java/com/example/filedownloaderHatori/MainActivity.kt
@@ -4,9 +4,12 @@ import android.app.Activity
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.os.Environment
+import android.provider.DocumentsContract
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.annotation.RequiresApi
@@ -18,6 +21,7 @@ import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
 import java.net.HttpURLConnection
+import java.net.URI
 import java.net.URL
 import java.text.SimpleDateFormat
 import java.util.Date
@@ -29,7 +33,7 @@ class MainActivity : AppCompatActivity() {
         Environment.getExternalStorageDirectory().getPath() + "/hatori_picture"
 
     companion object {
-        private const val REQUEST_GALLERY_TAKE = 2
+        private const val REQUEST_IMAGE_TAKE = 2
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
@@ -37,7 +41,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         // ダイアログを表示
         displayDialog()
         // URLから画像をダウンロード
@@ -56,7 +59,7 @@ class MainActivity : AppCompatActivity() {
     private fun displayDialog() {
         // 既存ディレクトリの有無を調べる
         if (File(directoryPath).isDirectory) return
-
+        // ダイアログ出力
         val builder = AlertDialog.Builder(this).apply {
             setMessage("ストレージへのアクセス許可")
             setPositiveButton("許可する") { _, _ ->
@@ -77,14 +80,14 @@ class MainActivity : AppCompatActivity() {
             // 既存ディレクトリの有無を調べる
             val myDirectory = File(directoryPath)
             if (!myDirectory.isDirectory) myDirectory.mkdir()
-        } catch (error: SecurityException) {
+        } catch (e: SecurityException) {
             // ファイルに書き込み用のパーミッションが無い場合など
-            error.printStackTrace()
-        } catch (error: IOException) {
+            e.printStackTrace()
+        } catch (e: IOException) {
             // 何らかの原因で誤ってディレクトリを2回作成してしまった場合など
-            error.printStackTrace()
-        } catch (error: Exception) {
-            error.printStackTrace()
+            e.printStackTrace()
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
     }
 
@@ -128,28 +131,33 @@ class MainActivity : AppCompatActivity() {
     private fun toGallery() {
         val intent = Intent(Intent.ACTION_PICK)
         intent.type = "image/*"
-        startActivityForResult(intent, REQUEST_GALLERY_TAKE)
+        startActivityForResult(intent, REQUEST_IMAGE_TAKE)
     }
 
     private fun toDocument() {
-        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
-        intent.type = "*/*"
-        startActivityForResult(intent, REQUEST_GALLERY_TAKE)
-    }
-
-    private fun clear() {
-        binding.URLInputField.setText("")
-        binding.image.setImageDrawable(null)
+        var test = Uri.fromFile(File(directoryPath))
+        val uri = Uri.parse(test.toString())
+        Log.d("Log", "$uri")
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT, uri)
+        intent.type = "image/*"
+        startActivityForResult(intent, REQUEST_IMAGE_TAKE)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         when (requestCode) {
-            REQUEST_GALLERY_TAKE -> {
-                if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_GALLERY_TAKE) {
+            REQUEST_IMAGE_TAKE -> {
+                if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_IMAGE_TAKE) {
+                    // 遷移先の画面から画像データを取得して表示
                     binding.image.setImageURI(data?.data)
+                    Toast.makeText(applicationContext, "画像を取得しました", Toast.LENGTH_SHORT)
                 }
             }
         }
+    }
+
+    private fun clear() {
+        binding.URLInputField.setText("")
+        binding.image.setImageDrawable(null)
     }
 }
